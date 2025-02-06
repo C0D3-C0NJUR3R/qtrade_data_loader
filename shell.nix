34c43b1eb0ac8cc1c86e2a3df22c69324cd66b61
alexandra <accounts@cons.dev>
2025-02-06 14:35:07 -0800

    upgraded to python 312

--- shell.nix
+++ shell.nix
@@ -1,15 +1,15 @@
 # shell.nix
 let
   # We pin to a specific nixpkgs commit for reproducibility.
-  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
+  # Last updated: 2025-02-06. Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/030ba1976b7c0e1a67d9716b17308ccdab5b381e.tar.gz") {};
   database_src = (builtins.fetchGit {
     url = "git@github.com:C0D3-C0NJUR3R/qtrade_database.git";
     ref = "main";
     rev = "868da72f922589f841f80dcd3e8f2cf6cb063aa2";
     allRefs = false;
   });
-  python = pkgs.python311.override {
+  python = pkgs.python312.override {
     self = python;
     packageOverrides = pyfinal: pyprev: {
       alpaca-py = pyfinal.callPackage ./alpaca-py.nix { };
